4147766fbb9f134d35e5b9004a9286aaa799b2f9
harikrishnan ap <harihaphari111@gmail.com>
2025-06-17 16:04:37 +0530

    deleting duplicate folders

--- bankapp/serializers.py
+++
@@ -1,9 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-from .models import Branches
-
-
-class BranchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Branches
-        fields = ['id', 'name', 'district_id']
